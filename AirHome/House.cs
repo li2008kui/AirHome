b3043a38c0e3464bf477fc1b33ca83e1edc8ad08
li2008kui <445754369@qq.com>
2015-01-17 21:59:49 +0800

    * 添加字段和方法

--- AirHome/House.cs
+++ AirHome/House.cs
@@ -5,7 +5,45 @@ using System.Text;
 
 namespace AirHome
 {
-    class House
+    public class House
     {
+        /// <summary>
+        /// 住所名称
+        /// </summary>
+        private string Name { get; private set; }
+        /// <summary>
+        /// 是否是主要住所
+        /// </summary>
+        private bool IsPrimary { get; private set; }
+
+        /// <summary>
+        /// 默认构造方法
+        ///  <para>住所名称为NULL</para>
+        ///  <para>默认为主要住所</para>
+        /// </summary>
+        public House()
+        {
+            IsPrimary = true;
+        }
+
+        /// <summary>
+        /// 初始化住所
+        /// </summary>
+        /// <param name="name">住所名称</param>
+        /// <param name="isPrimary">是否是主要住所</param>
+        public House(string name, bool isPrimary = true)
+        {
+            Name = name;
+            IsPrimary = isPrimary;
+        }
+
+        /// <summary>
+        /// 更新住所名称
+        /// </summary>
+        /// <param name="name">住所名称</param>
+        public void UpdateName(string name)
+        {
+            Name = name;
+        }
     }
 }
