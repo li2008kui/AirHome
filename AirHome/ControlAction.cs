f138fdd54e8d5e8c8ba56af1ee37c97379dadfe6
li2008kui <445754369@qq.com>
2015-01-24 18:07:44 +0800

    * 完善控制动作行为开关方法

--- AirHome/ControlAction.cs
+++ AirHome/ControlAction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AirHome
@@ -15,19 +16,35 @@ namespace AirHome
         /// 消息ID
         ///     <para>UInt16类型，长度为2个字节</para>
         /// </param>
-        /// <param name="parameters">参数列表</param>
+        /// <param name="status">
+        /// 开关状态
+        ///     <para>0X00表示关闭，0X01表示打开</para>
+        /// </param>
         /// <returns></returns>
-        public Byte[] Switch(UInt64 devId, params Parameter[] parameters)
+        public static Byte[] Switch(UInt64 devId, Byte status)
         {
+            if (status != 0X00 && status != 0X01)
+            {
+                throw new FormatException("开关状态参数错误！0X00表示关闭，0X01表示打开。");
+            }
+
+            List<Byte> bl = new List<Byte>();
+            bl.Add(status);
+
+            List<Parameter> pl = new List<Parameter>();
+            pl.Add(new Parameter(ParameterType.Switch, bl));
+
             MessageBody mb = new MessageBody(
                 MessageId.Switch,
                 devId,
-                parameters.ToList());
+                pl);
+
             MessageHead mh = new MessageHead(
                 (UInt16)(mb.GetBody().Length),
                 MessageType.ServerToDevice,
                 Counter.Instance.SeqNumber++,
                 Crc.GetCrc(mb.GetBody()));
+
             return new Datagram(mh, mb).GetDatagram();
         }
     }
