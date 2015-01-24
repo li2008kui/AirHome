fc1b189dd59660cc7d72e4af4ea57c473ee4dd5b
li2008kui <445754369@qq.com>
2015-01-24 10:13:11 +0800

    + 添加动作行为类

---
+++ AirHome/Action.cs
@@ -0,0 +1,9 @@
+﻿namespace AirHome
+{
+    /// <summary>
+    /// 动作行为抽象基类
+    /// </summary>
+    public abstract class Action
+    {
+    }
+}
---
+++ AirHome/ControlAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace AirHome
+{
+    /// <summary>
+    /// 控制行为类
+    /// </summary>
+    public class ControlAction : Action
+    {
+        /// <summary>
+        /// 开关行为
+        /// </summary>
+        /// <param name="devId">设备ID</param>
+        /// <param name="parameters">参数列表</param>
+        /// <returns></returns>
+        public Byte[] Switch(UInt64 devId, params Parameter[] parameters)
+        {
+            Datagram dg = new Datagram();
+
+            MessageBody mb = new MessageBody();
+            mb.MsgId = MessageId.Switch;
+            mb.DevId = devId;
+            mb.PmtList = parameters.ToList();
+
+            MessageHead mh = new MessageHead();
+            mh.Length = (UInt16)(mb.GetBody().Length);
+
+            return dg.GetDatagram();
+        }
+    }
+}
