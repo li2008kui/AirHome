6651bfbad36bcfa92206ca80a5fabf3ce8ba006b
li2008kui <445754369@qq.com>
2015-01-25 12:24:25 +0800

    + 添加通过红绿蓝白分量调节设备RGBW功能的方法

--- AirHome/ControlAction.cs
+++ AirHome/ControlAction.cs
@@ -48,7 +48,7 @@ namespace AirHome
         /// </param>
         /// <param name="level">
         /// 亮度等级
-        ///     <para>范围：0X01~0XFF</para>
+        ///     <para>取值范围：0X01~0XFF</para>
         /// </param>
         /// <returns></returns>
         public static Byte[] Dimming(UInt64 devId, Byte level)
@@ -77,7 +77,7 @@ namespace AirHome
         /// </param>
         /// <param name="cool">
         /// 冷色温分量
-        ///     <para>范围：0X01~0XFF</para>
+        ///     <para>取值范围：0X00~0XFF</para>
         ///     <para>色温与亮度的关系：冷色温+暖色温=当前亮度</para>
         /// </param>
         /// <returns></returns>
@@ -93,8 +93,8 @@ namespace AirHome
         }
 
         /// <summary>
-        /// 对设备进行RGB调节操作
-        ///     <para>参数值为红（R）、绿（G）、蓝（B）和白（W）的分量</para>
+        /// 对设备进行RGBW调节操作
+        ///     <para>参数值为红色（R）、绿色（G）、蓝色（B）和白色（W）的分量组合</para>
         /// </summary>
         /// <param name="devId">
         /// 设备ID
@@ -102,10 +102,10 @@ namespace AirHome
         /// </param>
         /// <param name="rgbw">
         /// 红绿蓝三基色和白色的分量
-        ///     <para>第1个字节表示红（R）的分量</para>
-        ///     <para>第2个字节表示绿（G）的分量</para>
-        ///     <para>第3个字节表示蓝（B）的分量</para>
-        ///     <para>第4个字节表示白（W）的分量</para>
+        ///     <para>第1个字节表示红色（R）的分量。取值范围：0X00~0XFF</para>
+        ///     <para>第2个字节表示绿色（G）的分量。取值范围：0X00~0XFF</para>
+        ///     <para>第3个字节表示蓝色（B）的分量。取值范围：0X00~0XFF</para>
+        ///     <para>第4个字节表示白色（W）的分量。取值范围：0X00~0XFF</para>
         /// </param>
         /// <returns></returns>
         public static Byte[] Rgbw(UInt64 devId, UInt32 rgbw)
@@ -116,6 +116,46 @@ namespace AirHome
             {
                 byteList.Add((Byte)(rgbw >> i));
             }
+
+            List<Parameter> pmtList = new List<Parameter>();
+            pmtList.Add(new Parameter(ParameterType.Rgbw, byteList));
+
+            return GetDatagram(MessageId.AdjustRgbw, devId, pmtList);
+        }
+
+        /// <summary>
+        /// 对设备进行RGBW调节操作
+        ///     <para>参数值为红色（R）、绿色（G）、蓝色（B）和白色（W）的分量</para>
+        /// </summary>
+        /// <param name="devId">
+        /// 设备ID
+        ///     <para>UInt64类型，长度为8个字节</para>
+        /// </param>
+        /// <param name="red">
+        /// 红色（R）的分量
+        ///     <para>取值范围：0X00~0XFF</para>
+        /// </param>
+        /// <param name="green">
+        /// 绿色（G）的分量
+        ///     <para>取值范围：0X00~0XFF</para>
+        /// </param>
+        /// <param name="blue">
+        /// 蓝色（B）的分量
+        ///     <para>取值范围：0X00~0XFF</para>
+        /// </param>
+        /// <param name="white">
+        /// 白色（W）的分量
+        ///     <para>取值范围：0X00~0XFF</para>
+        ///     <para>如果没有白光，默认为0X00</para>
+        /// </param>
+        /// <returns></returns>
+        public static Byte[] Rgbw(UInt64 devId, Byte red, Byte green, Byte blue, Byte white = 0X00)
+        {
+            List<Byte> byteList = new List<Byte>();
+            byteList.Add(red);
+            byteList.Add(green);
+            byteList.Add(blue);
+            byteList.Add(white);
 
             List<Parameter> pmtList = new List<Parameter>();
             pmtList.Add(new Parameter(ParameterType.Rgbw, byteList));
