8b0075679f346615445f89ebe464f32c43e11040
li2008kui <445754369@qq.com>
2015-01-27 14:49:54 +0800

    + 添加定位设备的方法

--- AirHome/ConfigAction.cs
+++ AirHome/ConfigAction.cs
@@ -23,5 +23,28 @@ namespace AirHome
 
             return GetDatagram(MessageId.SearchDevice, 0X0000000000000000, pmtList);
         }
+
+        /// <summary>
+        /// 定位设备
+        /// </summary>
+        /// <param name="devId">
+        /// 设备ID
+        ///     <para>UInt64类型，长度为8个字节</para>
+        /// </param>
+        /// <param name="circuitNo">
+        /// 回路（通道）编号
+        ///     <para>取值范围：0X01~0XFF；若为0X00，则表示所有回路</para>
+        /// </param>
+        /// <returns></returns>
+        public static Byte[] Locate(UInt64 devId, Byte circuitNo)
+        {
+            List<Byte> byteList = new List<Byte>();
+            byteList.Add(circuitNo);
+
+            List<Parameter> pmtList = new List<Parameter>();
+            pmtList.Add(new Parameter(ParameterType.CircuitNo, byteList));
+
+            return GetDatagram(MessageId.LocateDevice, devId, pmtList);
+        }
     }
 }
