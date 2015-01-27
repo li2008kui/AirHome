1a3f1c0a43b22e75af36c380bf0b464acda94908
li2008kui <445754369@qq.com>
2015-01-27 14:28:04 +0800

    + 添加配置动作行为类并添加搜索设备的方法

---
+++ AirHome/ConfigAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirHome
+{
+    /// <summary>
+    /// 配置行为类
+    /// </summary>
+    public class ConfigAction : Action
+    {
+        /// <summary>
+        /// 搜索设备
+        ///     <para>广播UID地址：0X0000000000000000</para>
+        /// </summary>
+        /// <returns></returns>
+        public static Byte[] Search()
+        {
+            List<Byte> byteList = new List<Byte>();
+            byteList.Add(0X00);
+
+            List<Parameter> pmtList = new List<Parameter>();
+            pmtList.Add(new Parameter(ParameterType.None, byteList));
+
+            return GetDatagram(MessageId.SearchDevice, 0X0000000000000000, pmtList);
+        }
+    }
+}
