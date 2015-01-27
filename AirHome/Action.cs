3d6222367382ce87d133ef6bec519185b2df8065
li2008kui <445754369@qq.com>
2015-01-27 14:25:23 +0800

    *+ 动作行为类改为非抽象类并添加获取消息报文字节数组的方法

--- AirHome/Action.cs
+++ AirHome/Action.cs
@@ -1,9 +1,36 @@
-﻿namespace AirHome
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirHome
 {
     /// <summary>
-    /// 动作行为抽象基类
+    /// 动作行为基类
     /// </summary>
-    public abstract class Action
+    public class Action
     {
+        /// <summary>
+        /// 获取消息报文字节数组
+        /// </summary>
+        /// <param name="msgId">消息ID</param>
+        /// <param name="devId">设备ID</param>
+        /// <param name="pmtList">参数列表</param>
+        /// <returns></returns>
+        protected static byte[] GetDatagram(MessageId msgId, UInt64 devId, List<Parameter> pmtList)
+        {
+            MessageBody mb = new MessageBody(
+                msgId,
+                devId,
+                pmtList);
+
+            Byte[] msgBody = mb.GetBody();
+
+            MessageHead mh = new MessageHead(
+                (UInt16)(msgBody.Length),
+                MessageType.ServerToDevice,
+                Counter.Instance.SeqNumber++,
+                Crc.GetCrc(msgBody));
+
+            return new Datagram(mh, mb).GetDatagram();
+        }
     }
 }
