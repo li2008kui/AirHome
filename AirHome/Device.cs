115a28d5eed105001fce95126b675a30d6999af0
li2008kui <445754369@qq.com>
2015-01-28 15:34:20 +0800

    + 完善设备类

--- AirHome/Device.cs
+++ AirHome/Device.cs
@@ -1,9 +1,50 @@
-﻿namespace ThisCoder.AirHome
+﻿using System;
+using System.Net;
+
+namespace ThisCoder.AirHome
 {
     /// <summary>
     /// 设备类
     /// </summary>
     public class Device
     {
+        /// <summary>
+        /// 设备ID
+        ///     <para>UInt64类型，长度为8个字节</para>
+        /// </summary>
+        public UInt64 DevId { get; set; }
+
+        /// <summary>
+        /// 设备名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 设备描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 设备MAC地址
+        /// </summary>
+        public UInt64 MacAddress { get; set; }
+
+        /// <summary>
+        /// 设备IP地址
+        /// </summary>
+        public IPAddress IPAddress { get; set; }
+
+        /// <summary>
+        /// 通过设备ID初始化设备对象
+        /// </summary>
+        /// <param name="devId">
+        /// 设备ID
+        ///     <para>UInt64类型，长度为8个字节</para>
+        /// </param>
+        public Device(UInt64 devId)
+        {
+            DevId = devId;
+            Name = devId.ToString();
+        }
     }
 }
