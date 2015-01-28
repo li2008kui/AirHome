9f8330ad70cbdcf6dfb6a9f4d7b0fca4df7cd6c5
li2008kui <445754369@qq.com>
2015-01-28 14:54:21 +0800

    + 对动作行为类添加相关操作方法

--- AirHome/Action.cs
+++ AirHome/Action.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ThisCoder.AirHome
 {
@@ -48,7 +50,7 @@ namespace ThisCoder.AirHome
         /// </param>
         /// <param name="pmtList">参数列表</param>
         /// <returns></returns>
-        protected byte[] GetDatagram(MessageId msgId, List<Parameter> pmtList)
+        protected Byte[] GetDatagram(MessageId msgId, List<Parameter> pmtList)
         {
             // 将回路编号添加到字节列表
             List<Byte> byteList = new List<Byte>();
@@ -77,6 +79,117 @@ namespace ThisCoder.AirHome
 
             // 返回消息报文字节数组
             return new Datagram(mh, mb).GetDatagram();
+        }
+
+        /// <summary>
+        /// 通过十六进制字符串获取字节数组
+        /// </summary>
+        /// <param name="hexString">十六进制字符串</param>
+        /// <param name="separator">
+        /// 分隔符
+        ///     <para>默认为空字符</para>
+        /// </param>
+        /// <returns></returns>
+        private Byte[] GetByteArray(string hexString, string separator = "")
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            if (!string.IsNullOrEmpty(hexString))
+            {
+                if (Regex.IsMatch(hexString.Replace(separator, "").Trim(), "^[0-9A-Fa-F]+$"))
+                {
+                    if (hexString.IndexOf(separator) >= 0)
+                    {
+                        foreach (var item in hexString.Split(new string[] { separator }, StringSplitOptions.RemoveEmptyEntries))
+                        {
+                            byteList.Add(Convert.ToByte(item, 16));
+                        }
+                    }
+                    else
+                    {
+                        hexString = hexString.Trim();
+                        hexString = ((hexString.Length % 2) != 0) ? ("0" + hexString) : hexString;
+
+                        for (int i = 0; i < hexString.Length; i += 2)
+                        {
+                            byteList.Add(Convert.ToByte(hexString.Substring(i, 2), 16));
+                        }
+                    }
+                }
+                else
+                {
+                    byteList.Add(0X00);
+                }
+            }
+            else
+            {
+                byteList.Add(0X00);
+            }
+
+            return byteList.ToArray();
+        }
+
+        /// <summary>
+        /// 通过“消息ID”、“参数类型”和“参数值字节列表”执行操作
+        /// </summary>
+        /// <param name="msgId">消息ID的枚举值</param>
+        /// <param name="pmtType">参数的类型枚举值</param>
+        /// <param name="pmtValueByteList">参数值字节列表</param>
+        /// <returns></returns>
+        public Byte[] Operate(MessageId msgId, ParameterType pmtType, List<Byte> pmtValueByteList)
+        {
+            Parameter parameter = new Parameter(pmtType, pmtValueByteList);
+            return Operate(msgId, parameter);
+        }
+
+        /// <summary>
+        /// 通过“消息ID”、“参数类型”和“字节类型的参数值”执行操作
+        /// </summary>
+        /// <param name="msgId">消息ID的枚举值</param>
+        /// <param name="pmtType">参数的类型枚举值</param>
+        /// <param name="pmtValueByte">字节类型的参数值</param>
+        /// <returns></returns>
+        public Byte[] Operate(MessageId msgId, ParameterType pmtType, Byte pmtValueByte)
+        {
+            List<Byte> byteList = new List<Byte>();
+            byteList.Add(pmtValueByte);
+
+            Parameter parameter = new Parameter(pmtType, byteList);
+            return Operate(msgId, parameter);
+        }
+
+        /// <summary>
+        /// 通过“消息ID”、“参数类型”和“字符串类型的参数值”执行操作
+        /// </summary>
+        /// <param name="msgId">消息ID的枚举值</param>
+        /// <param name="pmtType">参数的类型枚举值</param>
+        /// <param name="pmtValueString">字符串类型的参数值</param>
+        /// <param name="isHex">字符串类型的pmtValueString参数值是否是十六进制</param>
+        /// <param name="separator">
+        /// 分隔符
+        ///     <para>默认为空字符</para>
+        /// </param>
+        /// <returns></returns>
+        public Byte[] Operate(MessageId msgId, ParameterType pmtType, string pmtValueString, bool isHex = false, string separator = "")
+        {
+            Byte[] byteList = isHex ? GetByteArray(pmtValueString, separator) : Encoding.UTF8.GetBytes(pmtValueString);
+            List<Byte> pmtByteListValue = new List<byte>();
+            pmtByteListValue.AddRange(byteList);
+
+            return Operate(msgId, pmtType, pmtByteListValue);
+        }
+
+        /// <summary>
+        /// 通过“消息ID”和“参数结构体对象”执行操作
+        /// </summary>
+        /// <param name="msgId">消息ID的枚举值</param>
+        /// <param name="parameter">参数结构体对象</param>
+        /// <returns></returns>
+        public Byte[] Operate(MessageId msgId, Parameter parameter)
+        {
+            List<Parameter> pmtList = new List<Parameter>();
+            pmtList.Add(parameter);
+            return GetDatagram(msgId, pmtList);
         }
     }
 }
