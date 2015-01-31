10687efdd0b493af9d560be940edbd086da2af0f
li2008kui <445754369@qq.com>
2015-01-31 21:06:36 +0800

    + 添加获取开关状态的方法

--- AirHome/StateAction.cs
+++ AirHome/StateAction.cs
@@ -24,5 +24,14 @@ namespace ThisCoder.AirHome
         ///     <para>取值范围：0X01~0XFF；若为0X00，则表示所有回路，默认值为0X00</para>
         /// </param>
         public StateAction(UInt64 devId = 0X0000000000000000, Byte circuitNo = 0X00) : base(devId, circuitNo) { }
+
+        /// <summary>
+        /// 获取开关状态
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] SwitchState()
+        {
+            return GetDatagram(MessageId.StateSwitch, new Parameter(ParameterType.None, 0X00));
+        }
     }
 }
