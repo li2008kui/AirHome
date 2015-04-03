a8ac551396ab8566226b54c5cdfc56d0e94bc42e
li2008kui <445754369@qq.com>
2015-04-03 17:45:02 +0800

    * 完善数据采集行为类中的相关方法

--- AirHome/StateAction.cs
+++ AirHome/StateAction.cs
@@ -23,101 +23,182 @@ namespace ThisCoder.AirHome
         public StateAction(UInt64 devId = 0X0000000000000000, Byte ChannelNo = 0X00) : base(devId, ChannelNo) { }
 
         /// <summary>
-        /// 搜索设备及通道信息的命令
+        /// 搜索模块及通道信息的命令
         ///     <para>广播UID地址：0X0000000000000000</para>
         /// </summary>
         /// <returns></returns>
-        public Byte[] Search()
+        public Byte[] StateModuleOrChannelSearchCommand()
         {
             return GetDatagram(MessageId.StateModuleOrChannelSearch, new Parameter(ParameterType.None, 0X00));
         }
 
         /// <summary>
-        /// 获取开关状态
+        /// 发送心跳包到模块的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] SwitchState()
+        public Byte[] StateHeartbeatPacketToModuleCommand()
+        {
+            return GetDatagram(MessageId.StateHeartbeatPacketToModule, new Parameter(ParameterType.None, 0X00));
+        }
+
+        /// <summary>
+        /// 获取模块或通道打开或关闭状态的命令
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] StateModuleOrChannelSwitchCommand()
         {
             return GetDatagram(MessageId.StateModuleOrChannelSwitch, new Parameter(ParameterType.ChannelNo, ChannelNo));
         }
 
         /// <summary>
-        /// 获取亮度等级
+        /// 获取模块或通道亮度等级的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] BrightnessState()
+        public Byte[] StateModuleOrChannelBrightnessCommand()
         {
             return GetDatagram(MessageId.StateModuleOrChannelBrightness, new Parameter(ParameterType.ChannelNo, ChannelNo));
         }
 
         /// <summary>
-        /// 获取色温数据
+        /// 获取模块或通道色温数据的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] ColorTemperatureState()
+        public Byte[] StateModuleOrChannelColorTemperatureCommand()
         {
             return GetDatagram(MessageId.StateModuleOrChannelColorTemperature, new Parameter(ParameterType.ChannelNo, ChannelNo));
         }
 
         /// <summary>
-        /// 获取红绿蓝白（RGBW）颜色数据
+        /// 获取模块或通道红绿蓝白（RGBW）颜色数据的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] RgbwState()
+        public Byte[] StateModuleOrChannelRgbwCommand()
         {
             return GetDatagram(MessageId.StateModuleOrChannelRgbw, new Parameter(ParameterType.ChannelNo, ChannelNo));
         }
 
         /// <summary>
-        /// 获取设备分区代码
+        /// 获取模块或通道分区代码的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] PartitionState()
+        public Byte[] StateModuleOrChannelPartitionCodeCommand()
         {
             return GetDatagram(MessageId.StateModuleOrChannelPartitionCode, new Parameter(ParameterType.ChannelNo, ChannelNo));
         }
 
         /// <summary>
-        /// 获取设备名称
+        /// 获取模块或通道分区名称的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] DeviceName()
+        public Byte[] StateModuleOrChannelPartitionNameCommand()
+        {
+            return GetDatagram(MessageId.StateModuleOrChannelPartitionName, new Parameter(ParameterType.ChannelNo, ChannelNo));
+        }
+
+        /// <summary>
+        /// 获取模块或通道名称的命令
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] StateModuleOrChannelNameCommand()
         {
             return GetDatagram(MessageId.StateModuleOrChannelName, new Parameter(ParameterType.ChannelNo, ChannelNo));
         }
 
         /// <summary>
-        /// 获取设备描述
+        /// 获取模块或通道描述的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] DeviceDescription()
+        public Byte[] StateModuleOrChannelDescriptionCommand()
         {
             return GetDatagram(MessageId.StateModuleOrChannelDescription, new Parameter(ParameterType.ChannelNo, ChannelNo));
         }
 
         /// <summary>
-        /// 获取通信类型
+        /// 获取模块或通道图片名称或地址的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] CommunicationType()
+        public Byte[] StateModuleOrChannelImageCommand()
+        {
+            return GetDatagram(MessageId.StateModuleOrChannelImage, new Parameter(ParameterType.ChannelNo, ChannelNo));
+        }
+
+        /// <summary>
+        /// 获取模块通信类型的命令
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] StateModuleCommunicationTypeCommand()
         {
             return GetDatagram(MessageId.StateModuleCommunicationType, new Parameter(ParameterType.None, 0X00));
         }
 
         /// <summary>
-        /// 获取设备IP地址
+        /// 获取WiFi模块的运行模式的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] IpAddress()
+        public Byte[] StateModuleWifiRunModeCommand()
+        {
+            return GetDatagram(MessageId.StateModuleWifiRunMode, new Parameter(ParameterType.None, 0X00));
+        }
+
+        /// <summary>
+        /// 获取模块的无线网络名称SSID和信号强度的命令
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] StateModuleSsidAndPowerCommand()
+        {
+            return GetDatagram(MessageId.StateModuleSsidAndPower, new Parameter(ParameterType.None, 0X00));
+        }
+
+        /// <summary>
+        /// 获取模块附近的无线网络名称SSID和信号强度的命令
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] StateModuleNearbySsidAndPowerCommand()
+        {
+            return GetDatagram(MessageId.StateModuleNearbySsidAndPower, new Parameter(ParameterType.None, 0X00));
+        }
+
+        /// <summary>
+        /// 获取模块IP地址的命令
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] StateModuleIpAddressCommand()
         {
             return GetDatagram(MessageId.StateModuleIpAddress, new Parameter(ParameterType.None, 0X00));
         }
 
         /// <summary>
-        /// 获取设备MAC地址
+        /// 获取模块网关地址的命令
         /// </summary>
         /// <returns></returns>
-        public Byte[] MacAddress()
+        public Byte[] StateModuleGatewayAddressCommand()
+        {
+            return GetDatagram(MessageId.StateModuleGatewayAddress, new Parameter(ParameterType.None, 0X00));
+        }
+
+        /// <summary>
+        /// 获取模块子网掩码的命令
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] StateModuleSubnetMaskCommand()
+        {
+            return GetDatagram(MessageId.StateModuleSubnetMask, new Parameter(ParameterType.None, 0X00));
+        }
+
+        /// <summary>
+        /// 获取模块DNS地址的命令
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] StateModuleDnsAddressCommand()
+        {
+            return GetDatagram(MessageId.StateModuleDnsAddress, new Parameter(ParameterType.None, 0X00));
+        }
+
+        /// <summary>
+        /// 获取模块MAC地址的命令
+        /// </summary>
+        /// <returns></returns>
+        public Byte[] StateModuleMacAddressCommand()
         {
             return GetDatagram(MessageId.StateModuleMacAddress, new Parameter(ParameterType.None, 0X00));
         }
