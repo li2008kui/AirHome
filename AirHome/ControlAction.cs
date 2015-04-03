bb7e359fb33c2d11766a688e888a16b580b78eb5
li2008kui <445754369@qq.com>
2015-04-03 10:49:19 +0800

    * 重构控制动作行为类中的相关方法

--- AirHome/ControlAction.cs
+++ AirHome/ControlAction.cs
@@ -5,7 +5,7 @@ using System.Drawing;
 namespace ThisCoder.AirHome
 {
     /// <summary>
-    /// 控制行为类
+    /// 控制动作行为类
     /// </summary>
     public class ControlAction : AirAction
     {
@@ -25,58 +25,72 @@ namespace ThisCoder.AirHome
         public ControlAction(UInt64 devId = 0X0000000000000000, Byte channelNo = 0X00) : base(devId, channelNo) { }
 
         /// <summary>
-        /// 定位设备
+        /// 定位模块或通道的命令
         /// </summary>
+        /// <param name="deviceType">设备类型</param>
         /// <returns></returns>
-        public Byte[] Locate()
+        public Byte[] ControlModuleOrChannelLocateCommand(DeviceType deviceType = DeviceType.Module)
         {
-            return GetDatagram(MessageId.ControlModuleOrChannelLocate, new Parameter(ParameterType.ChannelNo, ChannelNo));
+            return GetDatagram(MessageId.ControlModuleOrChannelLocate,
+                deviceType == DeviceType.Channel ?
+                new Parameter(ParameterType.ChannelNo, ChannelNo) :
+                new Parameter(ParameterType.None, 0X00));
         }
 
         /// <summary>
-        /// 对设备进行开关操作
+        /// 打开或关闭模块或通道的命令
         ///     <para>如继电器开关或将设备亮度调到0%</para>
         /// </summary>
         /// <param name="status">
         /// 开关状态
         ///     <para>0X00表示关闭，0X01表示打开</para>
         /// </param>
+        /// <param name="deviceType">设备类型</param>
         /// <returns></returns>
-        public Byte[] Switch(Byte status)
+        public Byte[] ControlModuleOrChannelSwitchCommand(Byte status, DeviceType deviceType = DeviceType.Module)
         {
-            if (status != 0X00 && status != 0X01)
-                throw new FormatException("开关状态参数错误！0X00表示关闭，0X01表示打开。");
+            if (status > 0X01)
+                status = 0X01;
 
-            return GetDatagram(MessageId.ControlModuleOrChannelSwitch,
-                new List<Parameter>{
-                    new Parameter(ParameterType.ChannelNo, ChannelNo),
-                    new Parameter(ParameterType.Switch, status)
-            });
+            List<Parameter> pmtList = new List<Parameter>();
+
+            if (deviceType == DeviceType.Channel)
+            {
+                pmtList.Add(new Parameter(ParameterType.ChannelNo, ChannelNo));
+            }
+
+            pmtList.Add(new Parameter(ParameterType.Switch, status));
+            return GetDatagram(MessageId.ControlModuleOrChannelSwitch, pmtList);
         }
 
         /// <summary>
-        /// 对设备进行调光操作
+        /// 调节模块或通道亮度的命令
         ///     <para>若调光范围为1%~100%，需要转换为0X01~0XFF</para>
         /// </summary>
         /// <param name="level">
         /// 亮度等级
         ///     <para>取值范围：0X01~0XFF</para>
         /// </param>
+        /// <param name="deviceType">设备类型</param>
         /// <returns></returns>
-        public Byte[] Dimming(Byte level)
+        public Byte[] ControlModuleOrChannelBrightnessCommand(Byte level, DeviceType deviceType = DeviceType.Module)
         {
             if (level == 0X00)
                 level = 0X01;
 
-            return GetDatagram(MessageId.ControlModuleOrChannelBrightness,
-                new List<Parameter>{
-                    new Parameter(ParameterType.ChannelNo, ChannelNo),
-                    new Parameter(ParameterType.Brightness, level)
-            });
+            List<Parameter> pmtList = new List<Parameter>();
+
+            if (deviceType == DeviceType.Channel)
+            {
+                pmtList.Add(new Parameter(ParameterType.ChannelNo, ChannelNo));
+            }
+
+            pmtList.Add(new Parameter(ParameterType.Brightness, level));
+            return GetDatagram(MessageId.ControlModuleOrChannelBrightness, pmtList);
         }
 
         /// <summary>
-        /// 对设备进行色温调节操作
+        /// 调节模块或通道色温的命令
         ///     <para>参数值为冷色温分量</para>
         /// </summary>
         /// <param name="cool">
@@ -84,18 +98,23 @@ namespace ThisCoder.AirHome
         ///     <para>取值范围：0X00~0XFF</para>
         ///     <para>色温与亮度的关系：冷色温+暖色温=当前亮度</para>
         /// </param>
+        /// <param name="deviceType">设备类型</param>
         /// <returns></returns>
-        public Byte[] Toning(Byte cool)
+        public Byte[] ControlModuleOrChannelColorTemperatureCommand(Byte cool, DeviceType deviceType = DeviceType.Module)
         {
-            return GetDatagram(MessageId.ControlModuleOrChannelColorTemperature,
-                new List<Parameter>{
-                    new Parameter(ParameterType.ChannelNo, ChannelNo),
-                    new Parameter(ParameterType.ColorTemperature, cool)
-                });
+            List<Parameter> pmtList = new List<Parameter>();
+
+            if (deviceType == DeviceType.Channel)
+            {
+                pmtList.Add(new Parameter(ParameterType.ChannelNo, ChannelNo));
+            }
+
+            pmtList.Add(new Parameter(ParameterType.ColorTemperature, cool));
+            return GetDatagram(MessageId.ControlModuleOrChannelColorTemperature, pmtList);
         }
 
         /// <summary>
-        /// 对设备进行RGBW调节操作
+        /// 调节模块或通道RGBW的命令
         ///     <para>参数值为红色、绿色、蓝色和白色的分量组合</para>
         /// </summary>
         /// <param name="rgbw">
@@ -105,23 +124,27 @@ namespace ThisCoder.AirHome
         ///     <para>第3个字节表示蓝色（B）的分量。取值范围：0X00~0XFF</para>
         ///     <para>第4个字节表示白色（W）的分量。取值范围：0X00~0XFF</para>
         /// </param>
+        /// <param name="deviceType">设备类型</param>
         /// <returns></returns>
-        public Byte[] Rgbw(UInt32 rgbw)
+        public Byte[] ControlModuleOrChannelRgbwCommand(UInt32 rgbw, DeviceType deviceType = DeviceType.Module)
         {
             List<Byte> byteList = new List<Byte>();
+            List<Parameter> pmtList = new List<Parameter>();
 
             for (int i = 24; i >= 0; i -= 8)
                 byteList.Add((Byte)(rgbw >> i));
 
-            return GetDatagram(MessageId.ControlModuleOrChannelAdjustRgbw,
-                new List<Parameter>{
-                    new Parameter(ParameterType.ChannelNo, ChannelNo),
-                    new Parameter(ParameterType.Rgbw, byteList)
-                });
+            if (deviceType == DeviceType.Channel)
+            {
+                pmtList.Add(new Parameter(ParameterType.ChannelNo, ChannelNo));
+            }
+
+            pmtList.Add(new Parameter(ParameterType.Rgbw, byteList));
+            return GetDatagram(MessageId.ControlModuleOrChannelRgbw, pmtList);
         }
 
         /// <summary>
-        /// 对设备进行RGBW调节操作
+        /// 调节模块或通道RGBW的命令
         ///     <para>参数值为红色、绿色、蓝色和白色的分量</para>
         /// </summary>
         /// <param name="red">
@@ -141,74 +164,55 @@ namespace ThisCoder.AirHome
         ///     <para>取值范围：0X00~0XFF</para>
         ///     <para>如果没有白光，默认为0X00</para>
         /// </param>
+        /// <param name="deviceType">设备类型</param>
         /// <returns></returns>
-        public Byte[] Rgbw(Byte red, Byte green, Byte blue, Byte white = 0X00)
+        public Byte[] ControlModuleOrChannelRgbwCommand(Byte red, Byte green, Byte blue, Byte white = 0X00, DeviceType deviceType = DeviceType.Module)
         {
-            return GetDatagram(MessageId.ControlModuleOrChannelAdjustRgbw,
-                new List<Parameter>{
-                    new Parameter(ParameterType.ChannelNo, ChannelNo),
-                    new Parameter(ParameterType.Rgbw,
-                        new List<Byte>{
-                            red,
-                            green,
-                            blue,
-                            white
-                        })
-                });
+            List<Parameter> pmtList = new List<Parameter>();
+
+            if (deviceType == DeviceType.Channel)
+            {
+                pmtList.Add(new Parameter(ParameterType.ChannelNo, ChannelNo));
+            }
+
+            pmtList.Add(new Parameter(ParameterType.Rgbw,
+                new List<Byte>{
+                    red,
+                    green,
+                    blue,
+                    white
+                }));
+            return GetDatagram(MessageId.ControlModuleOrChannelRgbw, pmtList);
         }
 
         /// <summary>
-        /// 对设备进行RGB调节操作
+        /// 调节模块或通道RGBW的命令
         ///     <para>参数值为一种 ARGB 颜色（alpha、红色、绿色、蓝色）</para>
         /// </summary>
         /// <param name="color">一种 ARGB 颜色（alpha、红色、绿色、蓝色）</param>
+        /// <param name="deviceType">设备类型</param>
         /// <returns></returns>
-        public Byte[] Rgbw(Color color)
+        public Byte[] ControlModuleOrChannelRgbwCommand(Color color, DeviceType deviceType = DeviceType.Module)
         {
-            return GetDatagram(MessageId.ControlModuleOrChannelAdjustRgbw,
-                new List<Parameter>{
-                    new Parameter(ParameterType.ChannelNo, ChannelNo),
-                    new Parameter(ParameterType.Rgbw,
-                        new List<Byte>{
-                            color.R,
-                            color.G,
-                            color.B,
-                            color.A
-                        })
-                });
+            return ControlModuleOrChannelRgbwCommand(color.R, color.G, color.B, color.A, deviceType);
         }
 
         /// <summary>
-        /// 对设备进行RGB调节操作
+        /// 调节模块或通道RGBW的命令
         ///     <para>参数值为BMP图片</para>
         /// </summary>
         /// <param name="bitmap">BMP图片对象</param>
+        /// <param name="deviceType">设备类型</param>
         /// <returns></returns>
-        public List<Byte[]> Rgbw(Bitmap bitmap)
+        public IEnumerable<Byte[]> ControlModuleOrChannelRgbwCommand(Bitmap bitmap, DeviceType deviceType = DeviceType.Module)
         {
-            Color color;
-            List<Byte[]> byteArrayList = new List<byte[]>();
-
             for (int x = 0; x < bitmap.Width; x++)
             {
                 for (int y = 0; y < bitmap.Height; y++)
                 {
-                    color = bitmap.GetPixel(x, y);
-                    byteArrayList.Add(GetDatagram(MessageId.ControlModuleOrChannelAdjustRgbw,
-                        new List<Parameter>{
-                            new Parameter(ParameterType.ChannelNo, ChannelNo),
-                            new Parameter(ParameterType.Rgbw,
-                                new List<Byte>{
-                                    color.R,
-                                    color.G,
-                                    color.B,
-                                    color.A
-                                })
-                        }));
+                    yield return ControlModuleOrChannelRgbwCommand(bitmap.GetPixel(x, y), deviceType);
                 }
             }
-
-            return byteArrayList;
         }
     }
 }
